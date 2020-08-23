af724304cdce5bbec279747dce1b9bdc9176b4c9
Mateus Kamoei <mkamoei@kambda.com>
2020-08-23 16:26:01 -0300

    Initial Commit

---
+++ Table View Use Cases/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Table View Use Cases
+//
+//  Created by Mateus Kamoei on 23/08/20.
+//  Copyright © 2020 Kobe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
